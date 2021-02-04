368da03391b8e3a0ab71da0389ed88172b73261b
deepbodra96 <deepbodra96@gmail.com>
2021-02-04 11:24:06 -0500

    theoretical peak memory bandwidth

--- Assignment 1/deviceQuery.cu
+++ Assignment 1/deviceQuery.cu
@@ -18,11 +18,16 @@ int main (int argc, char *argv[])
      cudaCheck( cudaGetDeviceProperties(&prop, devId));
      std::cout << "Device id: " << devId << std::endl;
      std::cout << "Device name: " << prop.name << std::endl;
-
+     
 /*@  Calculate and print the theoretical peak bandwidth of the
      CUDA device.  You will need to obtain additional information
      from prop.  See the documentation 
 */
+     std::cout << "Device memory clock rate: " << prop.memoryClockRate << " KHz" << std::endl;
+     std::cout << "Device memory bus width: " << prop.memoryBusWidth << " bits" << std::endl;
+     float peakBandwidth = 2.0 * prop.memoryClockRate * prop.memoryBusWidth / 8.0 * 1e-6;
+     std::cout << "Device theoretical peak bandwidth: " << peakBandwidth << " GB/s" << std::endl;
+
 
   }
   std::cout << "-------------------------------------\n";
